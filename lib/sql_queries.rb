531a8ca5f1ae365140691c6c2266e2d11ff8355e
John Carter <johncarter352@gmail.com>
2020-09-28 08:49:35 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,11 +23,11 @@ def selects_most_prominent_color_and_returns_with_count
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "COUNT temperamnet FROM bears WHERE temperamnet = 'goofy'"
+  "SELECT COUNT temperamnet FROM bears WHERE temperamnet = 'goofy'"
 end
 
 def selects_bear_that_killed_Tim
-  "SELECT * FROM bears WHERE name = 'NULL'"
+  "SELECT * FROM bears WHERE name = 'null'"
 end
 
 #INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
