6cb9e4928d72ae248f94c8c4dadb30bce8981193
John Carter <johncarter352@gmail.com>
2020-09-28 08:28:12 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES 
+("Mr.Chocolate", 40, "Male", "brown", "calm", 0),
+("Rowdy", 10, "Male", "black", "intense", 1),
+("Tabitha", 6, "Female", "dark brown", "Nice", 1),
+("Sargeant Brown", 19, "Male", "Green", "Slimy", 0),
+("Melissa", 13, "Female", "dark brown", "goofy", 1),
+("Grinch", 2, "Male", "Black", "Grinchy", 1),
+("Wendy", 6, "Female", "Blue", "naive", 1),
+(NULL, 20, "Male", "black", "aggressive", 0);
--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,9 +1,9 @@
 def selects_all_female_bears_return_name_and_age
-  "Write your SQL query here"
+  "SELECT name, age FROM bears WHERE gender = 'F' "
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
-  "Write your SQL query here"
+  "SELECT * FROM bears ORDER BY name DESC;"
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
@@ -29,3 +29,13 @@ end
 def selects_bear_that_killed_Tim
   "Write your SQL query here"
 end
+
+#INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+#(1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
+#(2,"Rowdy", 10, "M", "black", "intense", 1),
+#(3,"Tabitha", 6, "F", "dark brown", "Nice", 1),
+#(4,"Sargeant Brown", 19, "M", "Green", "Slimy", 0),
+#(5,"Melissa", 13, "F", "dark brown", "goofy", 1),
+#(6,"Grinch", 2, "M", "Black", "Grinchy", 1),
+#(7,"Wendy", 6, "F", "Blue", "naive", 1),
+#(8,null, 20, "M", "black", "aggressive", 0);
